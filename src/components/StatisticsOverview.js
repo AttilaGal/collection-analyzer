f6028f9b99d1051092f071f29739e629dbdc5661
Attila Gal <attilagal@Attilas-MacBook-Pro-2.local>
2017-09-22 16:02:24 +0200

    added basic bar cahr twith dummy data

--- src/components/StatisticsOverview.js
+++ src/components/StatisticsOverview.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
-
+import { GamesByYear } from './charts/GamesByYear';
 
 export default class StatisticsOverview extends Component {
   render() {
-    return (<Header>{'statistics placeholder'}</Header>);
+    return (<GamesByYear />);
   }
 }
